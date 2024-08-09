e98dcf4e3daf199ee4240f0495efd0da138acddd
Apple <apple@Jyotis-MacBook.local>
2024-08-10 03:48:35 +1200

    added allowed hosts

--- EasyMeal/settings.py
+++ EasyMeal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fhvl-@515w@zhu0iak-tgx6=g*$z^u81%(_re)&kq)7wko!_9_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
