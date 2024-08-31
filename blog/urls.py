a0b5fdf812cbf35d6b355221776dbb61927731ab
chandan-segno <chandan@segnotech.com>
2024-08-31 11:59:31 +0530

    changes and busgs fixed

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 app_name = "blog"
 urlpatterns = [
         path('', views.blog, name='blog_page'),
-        path('blog/<slug:slug>/', views.blogDetails, name='blog_detail'),
+        path('<slug:slug>/', views.blogDetails, name='blog_detail'),
         path('blog/<slug:slug>/add_comment/', views.add_comment, name='add_comment'),
         path('service/', views.services, name='service'),
         path('subscribe/', views.subscribe, name='subscribe'),
