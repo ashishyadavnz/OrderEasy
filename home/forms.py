fefa716524c5d649e7f155666acd45abed98448d
manojSegnotech <manoj@segnotech.com>
2024-09-02 19:14:11 +0530

    my restro page and create form

--- home/forms.py
+++ home/forms.py
@@ -6,3 +6,18 @@ class ReservationForm(ModelForm):
 	class Meta:
 		model = Reservation
 		fields = ('name', 'email', 'phone', 'date', 'member')
+
+class RestaurantForm(ModelForm):
+	class Meta:
+		model = Restaurant
+		fields = ('title', 'logo', 'image', 'phone','email', 'found','city','postcode','address','website','latitude','longitude','facebook','twitter','instagram','linkedin','vip','source')
+
+	def __init__(self, *args, **kwargs):
+		user = kwargs.pop('user', None)
+		super(RestaurantForm, self).__init__(*args, **kwargs)
+		if user:
+			self.fields['city'].initial = user.city
+			self.fields['postcode'].initial = user.postcode
+			self.fields['address'].initial = user.address
+			self.fields['latitude'].initial = user.latitude
+			self.fields['longitude'].initial = user.longitude
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
         path('cuisine/<slug:cuisine_slug>/', views.restaurant, name='restaurants-cuisine'),
         path('<slug:slug>/<str:category>/', views.restaurantCard, name='restaurant-card-category'),
         path('<slug:slug>/', views.restaurantCard, name='restaurant-card'),
+        path('my-restaurant', views.myRestaurant, name='my_restaurant'),
+        path('add-restaurant', views.addRestaurant, name='add_restaurant'),
         path('', views.restaurant, name='restaurant'),
-
 ]
