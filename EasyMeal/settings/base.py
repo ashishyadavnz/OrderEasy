deaf020502d854c681523ff1fcb84fcf6b84165d
nitesh-segno <nitesh@segnotech.com>
2024-09-07 16:35:16 +0530

    add open close functionality on all restaurents page

--- EasyMeal/settings/base.py
+++ EasyMeal/settings/base.py
@@ -282,6 +282,6 @@ FCM_DJANGO_SETTINGS = {
 EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ordereasynz1@gmail.com' 
-EMAIL_HOST_PASSWORD = 'Genuine@77'
+EMAIL_HOST_PASSWORD = 'rajatdgvnklbduij'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
--- home/templatetags/custom_filter.py
+++ home/templatetags/custom_filter.py
@@ -1,5 +1,6 @@
 from django import template
-from django.utils import timezone  # Make sure this import is correct
+from django.utils import timezone
+import pytz
 
 register = template.Library()
 
@@ -13,7 +14,8 @@ def range_diff(value, diff):
 
 @register.filter
 def is_restaurant_open(start_time, end_time):
-    current_time = timezone.now().time()  # This now uses Django's timezone
+    auckland_tz = pytz.timezone('Pacific/Auckland')
+    current_time = timezone.now().astimezone(auckland_tz).time()
     if start_time and end_time:
         return start_time <= current_time <= end_time
     return False
