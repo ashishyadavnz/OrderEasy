b2881a1a82e044a931b42330ffa5c22e7cadd98a
chandan-segno <chandan@segnotech.com>
2024-09-17 11:06:52 +0530

    order type

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 	path('ajax/<slug:restro>/food-items/create/', views.create_food_item, name='create_food_item'),
     path('ajax/add-to-cart/', views.add_to_cart, name='add_to_cart'),
     path('ajax/clear_cart/', views.clear_cart, name='clear_cart'),
+    path('ajax/update_order_type/', views.update_order_type, name='update_order_type'),
     path('validate-voucher/', views.validate_voucher, name='validate_voucher'),
 	path('my-restaurant/<slug:restro>/food-items/', views.food_items, name='food_items'),
 	path('my-restaurant/<slug:restro>/orders/', views.orders_items, name='orders_items'),
