c58692dee9fc9aca143c91ac1eeaca80735619e0
nitesh-segno <nitesh@segnotech.com>
2024-10-29 19:51:59 +0530

    add production databse

--- EasyMeal/settings/production.py
+++ EasyMeal/settings/production.py
@@ -1,1 +1,14 @@
 from .base import *
+
+DEBUG = False
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'ordereasy',
+		'USER': 'ordereasy',
+		'PASSWORD': 'Jish1405#',
+		'HOST': 'ordereasydb.cj4y6c4c0ikd.ap-southeast-2.rds.amazonaws.com',
+		'PORT': '5432'
+	}
+}
